76d61f45464e7dd1375a537da0fa059667459f64
Jboy Flaga <flaga.jeremiah@gmail.com>
2019-04-09 01:42:12 +0800

    added files data and used ngFor to display them

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilesComponent implements OnInit {
 
+  files: Array<Object> = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.files.push({ name: 'Project-1.pptx', type: 'Powerpoint', last_modified: '12/09/2018'})
+    this.files.push({ name: 'Project-2.pptx', type: 'Powerpoint', last_modified: '01/01/2019'})
+    this.files.push({ name: 'Photos', type: 'Folder', last_modified: '04/05/2019'})
   }
-
+  
 }
