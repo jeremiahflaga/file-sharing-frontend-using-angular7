f8e0299e8a6ed9649899051a96eb7bad119d389b
Jboy Flaga <flaga.jeremiah@gmail.com>
2019-04-09 16:29:14 +0800

    added logic for selecting files

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -7,17 +7,44 @@ import { File } from '@app/_models';
 })
 export class FilesComponent implements OnInit {
 
-  files: Array<File> = [];
+  files: Array<FileVM> = [];
+  selectedFiles: Map<number, File> = new Map();
 
   constructor() { }
 
   ngOnInit() {
-    this.files.push({ id: 1, name: 'Project-1.pptx', type: 'Powerpoint', last_modified: new Date()})
-    this.files.push({ id: 2, name: 'Project-2.pptx', type: 'Powerpoint', last_modified: new Date()})
-    this.files.push({ id: 3, name: 'Photos', type: 'Folder', last_modified: new Date()})
+    this.files.push(new FileVM(new File(1, 'Project-1.pptx', 'Powerpoint', new Date())));
+    this.files.push(new FileVM(new File(2, 'Project-2.pptx', 'Powerpoint', new Date())));
+    this.files.push(new FileVM(new File(3, 'Photos', 'Folder', new Date())));
   }
 
   fileTrackByFn(index: number, item: File) {
     return item.id; // unique id corresponding to the item
+  }
+
+  fileCheckChanged(file: FileVM, event) {
+    if (event.target.checked) {      
+      this.selectedFiles.set(file.id, file);
+    } else {
+      if (this.selectedFiles.has(file.id)) {
+        this.selectedFiles.delete(file.id);
+      }
+    }
+  }
+}
+
+class FileVM {  
+  id: number;
+  name: string;
+  type: string;
+  last_modified: Date;
+
+  isSelected: boolean = false;
+
+  constructor(file: File) {
+    this.id = file.id;
+    this.name = file.name;
+    this.type = file.type;
+    this.last_modified = file.last_modified; 
   }
 }
